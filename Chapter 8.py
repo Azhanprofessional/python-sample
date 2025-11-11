25b7fd2a7d7ff62260b6f040f8bb6fe739f53f56
-Azhan-demo- <amohammedazhan7@gmail.com>
2025-11-11 16:39:46 +0530

    add a

--- Chapter 8.py
+++ Chapter 8.py
@@ -113,13 +113,23 @@ class Account:
         self.balance=bal
         self.account_no=acc
 
-    def debit(self,Amount):
-        self.
+    def debit(self,amount):
+        self.balance-=amount
+        print("Rs.",amount,"was debited ")
+        print("Total balance=",self.get_balance())
 
+    def credit(self,amount):
+        self.balance+=amount
+        print("Rs.",amount,"was credited ")
+        print("Total balance=",self.get_balance())
+
+    def get_balance(self):
+        return self.balance
 
 acc1=Account(20000,12345)
-print(acc1.balance)
-print(acc1.account_no)
+acc1.debit(1000)
+acc1.credit(10000)
+
 
     
 
